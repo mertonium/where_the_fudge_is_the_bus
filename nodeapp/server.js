eeba1158634901e619d6fb5d67b9e735a240c515
John Mertens <john.mertens@gmail.com>
2011-09-10 09:37:53 -0700

    Cleaned up code, refactoring as package

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,40 +1,37 @@
 var jsdom = require('jsdom');
 
-var stop_id = process.argv[2] || '16126';
+exports.getNextTrip = function(stop_id, options) {
+  var stop_id = process.argv[2] || '16126';
 
-var options = {
-  nexTripUrl: 'http://metrotransit.org/Mobile/Nextrip.aspx?stopnumber='+stop_id,
-  containerId: 'ctl00_mainContent_NexTripControl1_UPanelNextripOuter'
-};
+  var options = {
+    nexTripUrl: 'http://metrotransit.org/Mobile/Nextrip.aspx?stopnumber='+stop_id,
+    containerId: 'ctl00_mainContent_NexTripControl1_UPanelNextripOuter'
+  };
 
-var firstDom = jsdom.env(options.nexTripUrl,
-  ['http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js'],
-  function(errors, window) {
-    var routes = [];
-    var $panel = window.$('#'+options.containerId);
+  var theDom = jsdom.env(options.nexTripUrl,
+    ['http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js'],
+    function(errors, window) {
+      var routes = [], i = 0;
 
-    var retObj = {
-      next_departure: $panel.find('.countdown').text(),
-    };
-    
-    var rows = $panel.find('.row');
-console.log(rows.length);
-    if(rows.length) {
-      rows.forEach(function(row, idx) {
+      var $panel = window.$('#'+options.containerId);
+
+      var retObj = {
+        next_departure: $panel.find('.countdown').text(),
+      };
+
+      var rows = $panel.find('.nextripDepartures .data');
+      for(i=0; i < rows.length; i+=1){
+        $r = window.$(rows[i]);
         routes.push({
-          route_short_name: row.find('.col1').text(),
-          route_long_name: row.find('.col2').text(),
-          next_arrival: row.find('.col3').text()
+          route_short_name: $r.find('.col1').text(),
+          route_long_name: $r.find('.col2').text(),
+          next_arrival: $r.find('.col3').text()
         });
-      });
+      }
+
+      retObj.routes = routes;
+
+      console.log(retObj);
     }
-
-    retObj.routes = routes;
-
-    console.log('first jsdom callback');
-    console.log('-----------------');
-    console.log(window.$('#ctl00_mainContent_NexTripControl1_UPanelNextripOuter').html());
-    console.log('**********');
-    console.log(retObj);
-  }
-);
+  );
+};
