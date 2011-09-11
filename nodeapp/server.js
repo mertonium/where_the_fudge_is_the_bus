ee583c563fbbb056e562c5726cba08545716322a
John Mertens <john.mertens@gmail.com>
2011-09-11 00:41:38 -0700

    Completely reworked the nodeapp. Pulled the nextrip wrapper into its own node package, turned the current nodeapp into an Express app to serve up the site.

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,37 +1,34 @@
-var jsdom = require('jsdom');
+var nextrip = require('../../nextrip-node/lib/nextrip.js');
+var express = require('express');
 
-exports.getNextTrip = function(stop_id, options) {
-  var stop_id = process.argv[2] || '16126';
+var app = express.createServer();
 
-  var options = {
-    nexTripUrl: 'http://metrotransit.org/Mobile/Nextrip.aspx?stopnumber='+stop_id,
-    containerId: 'ctl00_mainContent_NexTripControl1_UPanelNextripOuter'
-  };
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
 
-  var theDom = jsdom.env(options.nexTripUrl,
-    ['http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js'],
-    function(errors, window) {
-      var routes = [], i = 0;
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-      var $panel = window.$('#'+options.containerId);
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
 
-      var retObj = {
-        next_departure: $panel.find('.countdown').text(),
-      };
 
-      var rows = $panel.find('.nextripDepartures .data');
-      for(i=0; i < rows.length; i+=1){
-        $r = window.$(rows[i]);
-        routes.push({
-          route_short_name: $r.find('.col1').text(),
-          route_long_name: $r.find('.col2').text(),
-          next_arrival: $r.find('.col3').text()
-        });
-      }
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
 
-      retObj.routes = routes;
+app.get('/nextrip/:stopid', function(req, res){
+  nextrip.getNextTrip(req.params.stopid, {}, function(departures) {
+    res.send(departures);
+  });
+});
 
-      console.log(retObj);
-    }
-  );
-};
+app.listen(3000);
