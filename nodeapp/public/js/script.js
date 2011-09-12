518c20b2d93f797feefeeb0a47b62b399305856f
John Mertens <john.mertens@gmail.com>
2011-09-11 19:44:19 -0700

    Whitespace

--- nodeapp/public/js/script.js
+++ nodeapp/public/js/script.js
@@ -40,9 +40,8 @@ var WTFIMB = {
           console.log(stops);
           // Loop through the stops and figure out what the next bus is
           for(; i < stops.length; i+=1) {
+
             s = stops[i].value;
-
-
             // Get the realtime info for each stop
             $.get('/nextrip/'+s.stop_id,{}, function(data) {
               $stopBlock = $('<li id="stop_'+s.stop_id+'" class="stop-block"><span class="stop-name">'+s.stop_name + ' (' + s.stop_desc +')</span><ul></ul></li>');
@@ -50,7 +49,7 @@ var WTFIMB = {
               var routes = data.routes;
               console.log(data);
 
-              // Loop through each arrival, building the 
+              // Loop through each arrival, building the
               for(; j < routes.length; j += 1) {
                 route = routes[j];
                 connector = (route.next_arrival.toLowerCase().indexOf('min') != -1) ? 'in' : 'at';
@@ -67,15 +66,15 @@ var WTFIMB = {
 
     var getPos = function(callback) {
       if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition( 
-          function(pos) { callback(pos); }, 
+        navigator.geolocation.getCurrentPosition(
+          function(pos) { callback(pos); },
           function(error) { callback(null, error); }
         );
       } else {
         console.log('Geolocation is not available.');
       }
     };
-  
+
     var findStops = function(pos, callback) {
       console.log('in findStops');
       var bbox = getBbox(pos);
@@ -85,52 +84,52 @@ var WTFIMB = {
         dataType: 'jsonp',
         success: function(_stops) {
           console.log(_stops);
-          _stops = _stops.rows;        
+          _stops = _stops.rows;
           $.each(_stops, function(idx, stop) {
               stop.distance = quickDist(pos.coords.latitude, pos.coords.longitude, stop.geometry.coordinates[1], stop.geometry.coordinates[0]);
           });
 
           // Sort the murals from closest to farthest
           _stops.sort(function(a, b) { return  a.distance - b.distance; });
-        
+
           // Only keep the closest 5
           _stops = _stops.slice(0,5);
-        
+
           //console.log(_stops);
           callback(_stops);
         }
       };
-    
+
       $.ajax(opts, function(data) {
         callback(data);
       });
     };
-  
+
     var getBbox = function(pos) {
       var factor = 0.016;  // About a mile...
       var bbox = [
-        pos.coords.longitude - factor, 
-        pos.coords.latitude - factor, 
-        pos.coords.longitude + factor, 
+        pos.coords.longitude - factor,
+        pos.coords.latitude - factor,
+        pos.coords.longitude + factor,
         pos.coords.latitude + factor
       ];
-    
-      return bbox.join(','); 
+
+      return bbox.join(',');
     };
-  
+
     // http://www.movable-type.co.uk/scripts/latlong.html
     var quickDist = function(lat1, lon1, lat2, lon2) {
       var R = 20902231; // radius of the earth in ft
-      var d = Math.acos(Math.sin(lat1.toRad())*Math.sin(lat2.toRad()) + 
+      var d = Math.acos(Math.sin(lat1.toRad())*Math.sin(lat2.toRad()) +
                         Math.cos(lat1.toRad())*Math.cos(lat2.toRad()) *
                         Math.cos(lon2.toRad()-lon1.toRad())) * R;
       return d;
     };
-  
+
     return {
       init: init
     }
-  }  
+  }
 })(WTFIMB);
 
 /** Converts numeric degrees to radians */
