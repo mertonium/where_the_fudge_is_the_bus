b22e4a16d8b06b3d047d0fa4da5556112690cb18
John Mertens <john.mertens@gmail.com>
2011-09-11 20:22:21 -0700

    Cleaned out comments.

--- massupload.js
+++ massupload.js
@@ -2,12 +2,12 @@ var csv     = require('csv'),
     cradle  = require('cradle');
 
 var docs = [];
-    
+
 var successCount = 0,
     failCount = 0,
     curproc = 0,
     throttle = 5;
-    
+
 var db = new(cradle.Connection)().database('msp_transit');
 
 csv()
@@ -15,24 +15,6 @@ csv()
   .on('data',function(data,index){
     data.type = 'stop_time';
     docs.push(data);
-    // while(curproc > throttle) { 
-    //   //setTimeout(function(){ 
-    //     console.log('waiting on stop # '+data.stop_id);
-    //   //}, 1000); 
-    // }
-    // 
-    // curproc += 1;
-    // console.log('saving '+data.stop_id+' curproc = '+curproc);
-    // db.save(data, function(err, res) {
-    //   console.log(res);
-    //   if(err) {
-    //     failCount += 1;
-    //     console.error(err);
-    //   } else {
-    //     successCount += 1;
-    //   }
-    //   curproc -= 1;
-    // });
   })
   .on('end',function(count){
     db.save(docs, function(err, res) {
@@ -45,8 +27,6 @@ csv()
         console.log(res);
       });
     });
-    // console.log(successCount + ' successes.');
-    // console.log(failCount + ' failures.');
   })
   .on('error',function(error){
     console.log(error.message);
